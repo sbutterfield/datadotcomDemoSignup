16156c28e83fba76728dece55f15ca44f473cb17
Shawn Butterfield <sbutter@gmail.com>
2011-10-24 23:44:33 -0700

    new rails app added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatadotcomDemoSignup::Application.config.secret_token = 'b0f564af331542a26e3b26b3c667cf72f73d282e52d6cf21951dc40033af8ccc62a88f057e1ed8276b1a4a5d1c8e9cb22af9a60dd5cde99c5f406558eacc0825'
